650d0ab3935d32043ef8222d128ea1e050792b6c
Atul <atul.kumar@wisdmlabs.com>
2023-02-02 01:10:39 +0530

    commit for creating js file

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,21 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+function myFunction(){
+    // window.location = "pricing.html"
+    let inputVal = document.getElementById("inputVal").value;
+    
+    if(inputVal==="classic 350")
+    {
+        window.location = "pricing.html";
+    }
+    else if(inputVal==="activa")
+    {
+        window.location = "pricing1.html";
+    }
+    else
+    {
+        alert("result not found");
+    }
+}
